6fa372cd1ec11263d29aa85d4129aee9a146101d
Matt Schroeder <matthew.schroeder@improving.com>
2023-03-30 20:53:02 -0500

    exercise 1: address sort and stringify

---
+++ src/main/java/org/improving/workshop/exercises/AddressSortAndStringify.java
@@ -0,0 +1,48 @@
+package org.improving.workshop.exercises;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+
+import static org.improving.workshop.Streams.*;
+
+/**
+ * Goals -
+ * 1. Rekey the address stream by state
+ * 2. Condense the address into a single string, removing identifiers (address & customer)
+ *      - String Format: "{line1}, {line2}, {citynm}, {state} {zip5}-{zip4} {countrycd}"
+ */
+@Slf4j
+public class AddressSortAndStringify {
+    // MUST BE PREFIXED WITH "kafka-workshop-"
+    public static final String OUTPUT_TOPIC = "kafka-workshop-addresses-by-state";
+
+    /**
+     * The Streams application as a whole can be launched like any normal Java application that has a `main()` method.
+     */
+    public static void main(final String[] args) {
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // configure the processing topology
+        configureTopology(builder);
+
+        // fire up the engines
+        startStreams(builder);
+    }
+
+    static void configureTopology(final StreamsBuilder builder) {
+//        builder
+//            .stream(TOPIC_DATA_DEMO_ADDRESSES, Consumed.with(Serdes.String(), SERDE_ADDRESS_JSON))
+//            .peek((addressId, address) -> log.info("Address Received: {}, {}", addressId, address))
+//
+//            // DESIRED FORMAT
+//            // "{line1}, {line2}, {citynm}, {state} {zip5}-{zip4} {countrycd}"
+//
+//            .peek((addressId, address) -> log.info("Address Processed: {}, {}", addressId, address))
+//            // NOTE: when using ccloud, the topic must exist or 'auto.create.topics.enable' set to true (dedicated cluster required)
+//            .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+    }
+}
---
+++ src/test/groovy/org/improving/workshop/exercises/AddressSortAndStringifySpec.groovy
@@ -0,0 +1,72 @@
+package org.improving.workshop.exercises
+
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.TestInputTopic
+import org.apache.kafka.streams.TestOutputTopic
+import org.apache.kafka.streams.TopologyTestDriver
+import org.improving.workshop.Streams
+import org.msse.demo.mockdata.customer.address.Address
+import spock.lang.Specification
+
+class AddressSortAndStringifySpec extends Specification {
+    TopologyTestDriver driver
+
+    // inputs
+    TestInputTopic<String, Address> addressInputTopic
+
+    // outputs - addressid, address (string)
+    TestOutputTopic<String, String> outputTopic
+
+    def 'setup'() {
+        // instantiate new builder
+        StreamsBuilder streamsBuilder = new StreamsBuilder()
+
+        // build the RemainingEventTickets topology (by reference)
+        AddressSortAndStringify.configureTopology(streamsBuilder)
+
+        // build the TopologyTestDriver
+        driver = new TopologyTestDriver(streamsBuilder.build(), Streams.buildProperties())
+
+        addressInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_ADDRESSES,
+                Serdes.String().serializer(),
+                Streams.SERDE_ADDRESS_JSON.serializer()
+        )
+
+        outputTopic = driver.createOutputTopic(
+                AddressSortAndStringify.OUTPUT_TOPIC,
+                Serdes.String().deserializer(),
+                Serdes.String().deserializer()
+        )
+    }
+
+    def 'cleanup'() {
+        // if this close doesn't run (test fails halfway through), subsequent tests may start on old state
+        // run the test and let it cleanup, then run the test again.
+        driver.close()
+    }
+
+    def "address stringify"() {
+        given: 'an address'
+        def addressId = "address-123"
+        def address = new Address(
+                addressId, "cust-678", "cd", "HOME", "111 1st St", "Apt 2",
+                "Minneapolis", "MN", "55419", "1234", "USA")
+
+        when: 'piping the address through the stream'
+        addressInputTopic.pipeInput(addressId, address)
+
+        then: 'reading the output records'
+        def outputRecords = outputTopic.readRecordsToList()
+
+        then: 'one record came through'
+        outputRecords.size() == 1
+
+        and: 'the key was changed to be the addresses state'
+        outputRecords.first().key() == address.state()
+
+        and: 'the value was stringified'
+        outputRecords.first().value() == "111 1st St, Apt 2, Minneapolis, MN 55419-1234 USA"
+    }
+}

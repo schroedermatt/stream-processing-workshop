5deef4d19db2eb1897a47ceb8bbb5ce688e8f4d7
Chuck55 <41636138+Chuck55@users.noreply.github.com>
2023-04-28 15:06:21 -0500

    Update ArtistTicketRatio.java

--- src/main/java/org/improving/workshop/project/ArtistTicketRatio.java
+++ src/main/java/org/improving/workshop/project/ArtistTicketRatio.java
@@ -4,4 +4,22 @@ package org.improving.workshop.project;
  * Class for solution to Question 1
  */
 public class ArtistTicketRatio {
+    public static final String OUTPUT_TOPIC = "kafka-artist-ticket-ratio";
+
+    /**
+      Inital Start
+      Kyle
+    */
+    public static void main(final String[] args) {
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // configure the processing topology
+        configureTopology(builder);
+
+        // fire up the engines
+        startStreams(builder);
+    }
+
+    static void configureTopology(final StreamsBuilder builder) {
+    }
 }
